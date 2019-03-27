af2af9a415527202fa4e453ca6418b18bb9ea6dd
KnifeParty12 <flashfunk01@gmail.com>
2019-03-28 02:53:34 +0530

    Added Front-Page Animations
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -4,7 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
 import {Link} from "react-router-dom";
-
+import {CityLogo} from "../ui/Icons";
 
 class Header extends Component {
     render() {
@@ -18,10 +18,15 @@ class Header extends Component {
                 borderBottom: '2px solid #00285e'
             }}
             >
-                <Toolbar styles={{display: 'flex'}}>
-                    <div styles={{flexGrow: 1}}>
+                <Toolbar style={{display: 'flex'}}>
+                    <div style={{flexGrow: 1}}>
                         <div className={"header_logo"}>
-                            LOGO
+                            <CityLogo
+                                link={true}
+                                linkTo={"/"}
+                                width={"70px"}
+                                height={"70px"}
+                            />
                         </div>
                     </div>
                     
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
+import {Switch, Route} from 'react-router-dom';
+
+
+import Home from './Components/home'
 
 const Routes = (props) => {
   return(
       <Layout>
-        second line
+          <Switch>
+            <Route exact component={Home} path={"/"}/>
+          </Switch>
       </Layout>
       )
 };
