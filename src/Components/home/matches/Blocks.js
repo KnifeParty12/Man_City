0fe351f96f42fd2e69dddf457cc9d98b56186ed9
KnifeParty12 <flashfunk01@gmail.com>
2019-03-28 04:29:18 +0530

    Added Matches
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/Components/home/matches/Blocks.js
+++ src/Components/home/matches/Blocks.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {firebaseMatches} from "../../../firebase";
 import {firebaseLooper,reverseArray} from "../../ui/misc";
+import MatchesBlock from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide';
 
 class Blocks extends Component {
 
@@ -19,12 +21,19 @@ class Blocks extends Component {
         })
     }
 
-    showMatches = () => (
-        <div>
-            match
-        </div>
-    );
-
+    showMatches = (matches) => (
+            matches ?
+                matches.map((match)=>(
+                    <Slide bottom key={match.id}>
+                    <div className={"item"}>
+                        <div className={"wrapper"}>
+                        <MatchesBlock match={match}/>
+                    </div>
+                    </div>
+                    </Slide>
+                ))
+                :null
+        );
 
     render() {
         console.log(this.state)
