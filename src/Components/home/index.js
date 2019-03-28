b9d235b5f0ace51847e8e4c5d9636e4a65e3a402
KnifeParty12 <flashfunk01@gmail.com>
2019-03-28 16:54:13 +0530

    Meet The Players component added
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Featured from './featured';
 import Matches from  './matches';
+import MeetPlayers from './meetPlayers';
+
+
 const Home = () => {
     return (
         <div className={"bck_blue"}>
             <Featured/>
             <Matches/>
+            <MeetPlayers/>
         </div>
     );
 };
